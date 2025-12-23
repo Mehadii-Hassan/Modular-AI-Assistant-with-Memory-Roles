6a873da1f1953ec175895192afe497a607e3ace6
Mehadii-Hassan <mehadi.cse01@gmail.com>
2025-12-23 17:03:40 +0600

    gemini engine

--- mehu/gemini_engine.py
+++ mehu/gemini_engine.py
@@ -3,18 +3,11 @@ import google.generativeai as genai
 class GeminiEngine:
     def __init__(self, api_key):
         genai.configure(api_key=api_key)
-        self.model = genai.GenerativeModel("gemini-2.5-flash")
 
-    def stream(self, prompt):
+    def generate(self, prompt):
         try:
-            response = self.model.generate_content(
-                prompt,
-                stream=True
-            )
-
-            for chunk in response:
-                if chunk.text:
-                    yield chunk.text
-
+            model = genai.GenerativeModel("gemini-pro")
+            response = model.generate_content(prompt)
+            return response.text
         except Exception as e:
-            yield f"\n❌ Gemini Error: {str(e)}"
+            return f"⚠️ Error: {str(e)}"
